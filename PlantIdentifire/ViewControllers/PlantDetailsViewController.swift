0be75942ba2a976930f7db7d989d941a127e9c65
Krishna Gorajiya <swainfo.krishna3@gmail.com>
2023-05-06 10:42:20 +0530

    - Onboarding image changed and details screen UI

---
+++ PlantIdentifire/ViewControllers/PlantDetailsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PlantDetailsViewController.swift
+//  PlantIdentifire
+//
+//  Created by Swain Info on 05/05/23.
+//
+
+import UIKit
+
+class PlantDetailsViewController: UIViewController {
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    
+
+}
